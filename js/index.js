a9afd82de162cccac9a85ecd496f4878b2e95553
Natalie Halawani <hala1224@gmail.com>
2018-12-23 00:47:34 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // your code here
+function getRepositories() {
+  const req = new XMLHttpRequest();
+  const username = document.getElementById('username').value;
+  req.addEventListener("load", displayRepositories);
+  req.open("GET",
+  'https://api.github.com/users/' + username + '/repos');
+  req.send();
+  return false;
+}
