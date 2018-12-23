ad603069bb164f932de4b5b9484e48e1099b5298
Natalie Halawani <hala1224@gmail.com>
2018-12-23 00:56:32 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -33,3 +33,24 @@ function displayRepositories(event, data) {
   xhr.open("GET", uri);
   xhr.send();
 }
+function displayCommits() {
+  const commits = JSON.parse(this.responseText);
+  const commitsList = `<ul>${commits.map(commit => '<li><h3>' + commit.commit.author.name +
+' (' + commit.author.login + ')</h3>' + commit.commit.message + '</li>').join('')}</ul>`;
+  document.getElementById("details").innerHTML = commitsList;
+}
+
+function getBranches(el) {
+  const repoName = el.dataset.repository;
+  const uri = "https://api.github.com" + "/repos/" + el.dataset.username + "/" + repoName + "/branches";
+  const xhr = new XMLHttpRequest();
+  xhr.addEventListener("load", displayBranches);
+  xhr.open("GET", uri);
+  xhr.send();
+}
+
+function displayBranches() {
+  const branches = JSON.parse(this.responseText);
+  const branchesList = `<ul>${branches.map(branch => '<li>' + branch.name + '</li>').join('')}</ul>`;
+  document.getElementById("details").innerHTML = branchesList;
+}
